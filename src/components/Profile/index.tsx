d7f05036556a714254f9b4d421916282a2509350
pedropalombo <pedro.palombo@hotmail.com>
2023-07-26 17:47:02 -0300

    Reordering DOM Elements & Exporting ProfileInfo as Component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,11 +1,16 @@
 import { Community } from "../Community";
 import { Friends } from "../Friends";
 import styles from "./Profile.module.css";
-import IconImage from "../../assets/images/profileIcon.svg"; 
+import IconImage from "../../assets/images/profileIcon.svg";
+import { ProfileInfo } from "../ProfileInfo";
 
 export const Profile = () => {
     return (
         <section className="mainContainer">
+            <section className="profileInfoContainer">
+                <ProfileInfo />
+            </section>
+
             <section className="profileDisplayContainer">
                 <div className="imageContainer">
                     <img src={IconImage}></img>
@@ -15,18 +20,19 @@ export const Profile = () => {
                     <span className="userExtras"></span>
                 </div>
             </section>
-            
+
             <button className="editProfileBtn"></button>
 
-            <section className="profileInfoContainer">
-                <span className="greetingsText"></span>
+            <section className="rightSideContainer">
+                <div className="friendsContainer">
+                    <Friends />
+                </div>
+
+                <div className="communityContainer">
+                    <Community />
+                </div>
             </section>
 
-            <section className="rightSideContainer">
-                <Friends />
-                <Community />
-            </section>
         </section>
-        
     );
 }
---
+++ src/components/ProfileInfo/index.tsx
@@ -0,0 +1,5 @@
+export function ProfileInfo() {
+    return (
+        
+    );
+}
